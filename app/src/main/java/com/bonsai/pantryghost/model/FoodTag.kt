3b424050b990de8115bb9785e4653f1726a433c9
Luke Bollwerk <lukas8@gmail.com>
2023-09-18 08:43:35 -0600

    make sure foodtag.name is unique

--- app/src/main/java/com/bonsai/pantryghost/model/FoodTag.kt
+++ app/src/main/java/com/bonsai/pantryghost/model/FoodTag.kt
@@ -1,9 +1,13 @@
 package com.bonsai.pantryghost.model
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "food_tag")
+@Entity(
+    tableName = "food_tag",
+    indices = [Index(value = ["name"], unique = true)]
+)
 data class FoodTag(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
